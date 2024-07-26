d8897654f0fc9cc0aee17ad8f4cfaa0744f457de
jvquaill <Jannes.Vanquaillie@ugent.be>
2024-07-26 13:59:52 +0200

    working tv magic remote

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.getElementById('alertButton').addEventListener('click', function() {
     alert('Button was clicked!');
 });
+let current_channel = 1;
+for (let index = 1; index < 6; index++) {
+    document.getElementById('button'+index).addEventListener('click', function() {
+        current_channel = (current_channel  +index)%5 ;
+        if(current_channel == 0) current_channel = 5;
+        document.getElementById('channel_number').innerText = "Channel # "+current_channel;
+    });  
+}
